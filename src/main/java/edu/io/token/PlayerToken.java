c1c97e7de245537f7c44ba6cacf1dc653c549eea
dbober97 <dorotabober97@gmail.com>
2025-11-17 18:55:39 +0100

    fixed repeating code in PlayerToken

--- src/main/java/edu/io/token/PlayerToken.java
+++ src/main/java/edu/io/token/PlayerToken.java
@@ -4,8 +4,8 @@ import edu.io.Board;
 public class PlayerToken extends Token {
 
     Board board;
-    private int col = 0;
-    private int row = 0;
+    private int col;
+    private int row;
 
     public enum Move{
         NONE,
@@ -15,58 +15,42 @@ public class PlayerToken extends Token {
         DOWN
     }
 
-        public PlayerToken(Board board) {
-            super(Label.PLAYER_TOKEN_LABEL);
-            this.board = board;
-            this.board.placeToken(this.col, this.row, this);
-    }
+    public PlayerToken(Board board) {
+        super(Label.PLAYER_TOKEN_LABEL);
+        this.board = board;
+        this.row = 0;
+        this.col = 0;
+        this.board.placeToken(this.col, this.row, this);
+     }
 
     public void move(Move dir)
     {
+        int newRow = this.row;
+        int newCol = this.col;
         switch (dir){
-            case NONE:
-                break;
-            case LEFT:
-            {
-                if(col > 0) {
-                    board.placeToken(col, row, new EmptyToken());
-                    col -= 1;
-                }
-                else throw new IllegalArgumentException("Cannot move outside the board");
-                board.placeToken(col, row, this);
+            case NONE: break;
+            case LEFT: {
+                newCol--;
                 break;
             }
-            case RIGHT:
-            {
-                if(this.col < this.board.size-1){
-                    board.placeToken(this.col, this.row, new EmptyToken());
-                    this.col += 1;
-                }
-                else throw new IllegalArgumentException("Cannot move outside the board");
-                board.placeToken(col, row, this);
+            case RIGHT: {
+                newCol++;
                 break;
             }
-            case UP:
-            {
-                if(row > 0){
-                    board.placeToken(col, row, new EmptyToken());
-                    row -= 1;
-                }
-                else throw new IllegalArgumentException("Cannot move outside the board");
-                board.placeToken(col, row, this);
+            case UP: {
+                newRow--;
                 break;
             }
-            case DOWN:
-            {
-                if(row < board.size-1){
-                    board.placeToken(col, row, new EmptyToken());
-                    row += 1;
-                }
-                else throw new IllegalArgumentException("Cannot move outside the board");
-                board.placeToken(col, row, this);
+            case DOWN: {
+                newRow++;
                 break;
             }
         }
+        if(newRow < 0 || newRow >= board.size || newCol < 0 || newCol >= board.size()) throw new IllegalArgumentException("Cannot move outside the board");
+        board.placeToken(this.col, this.row, new EmptyToken());
+        board.placeToken(newCol, newRow, this);
+        this.row = newRow;
+        this.col = newCol;
     }
 
     public Board.Coords pos(){
